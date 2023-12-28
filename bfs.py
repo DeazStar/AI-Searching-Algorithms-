dce2bd2391656b02aea5bc1025badb36b97239df
DeazStar <deadfromtheborn@gmail.com>
2023-12-28 13:11:47 +0300

    implementation of bfs

---
+++ bfs.py
@@ -0,0 +1,26 @@
+from graph import graph
+
+def dfs(graph, start_node, goal_node):
+  stack = []
+  visited = set()
+  path = {start_node: [start_node]}
+
+  stack.append(start_node)
+
+  while stack:
+    current_element = stack.pop()
+    visited.add(current_element)
+
+    if current_element == goal_node:
+      return path[current_element]
+
+    for neighbor in graph.get(current_element, []):
+      if neighbor not in visited:
+        stack.append(neighbor)
+        path[neighbor] = path[current_element] + [neighbor]
+
+start_node = 'A'
+goal_node = 'G'
+result_path = dfs(graph, start_node, goal_node)
+
+print(result_path)
